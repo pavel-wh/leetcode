ddb86d7862a0a9c144c4b6a1d5e740dfc8c7d114
pavel <pavel.wh@yandex.ru>
2021-10-26 03:24:32 +0300

    solved 5 task

--- 5.task.js
+++ 5.task.js
@@ -6,4 +6,31 @@ https://leetcode.com/problems/longest-palindromic-substring/
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {};
+var longestPalindrome = function (s) {
+  let res = '';
+
+  if (!s) return res;
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      let left = i;
+      let right = i + j;
+
+      while (left >= 0 && right <= s.length && s[left] == s[right]) {
+        left--;
+        right++;
+      }
+
+      let start = left + 1;
+      let end = right;
+
+      let substring = s.slice(start, end);
+
+      if (res.length < substring.length) {
+        res = substring;
+      }
+    }
+  }
+
+  return res;
+};
